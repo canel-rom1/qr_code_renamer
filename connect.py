8dfa2c464049be85403ffccd69a36cbf42d8d938
Patrick Hervieux <patrick.hervieux@museebolo.ch>
2021-03-06 18:39:16 +0100

    Return folder ID from a given folder's name

--- connect.py
+++ connect.py
@@ -34,3 +34,35 @@ def get_gdrive_service():
             pickle.dump(creds, token)
     service = build('drive', 'v3', credentials=creds)
     return service
+
+
+def get_folder_id(google_service, folder):
+    """Return list with a dict containing name and id of files from a given folder
+    Parameter google_service is a googleapiclient.discovery.Resource class
+    Parameter folder is a string containing folder name
+    """
+
+    # Fetch id of folder
+    results = google_service.files().list(
+        q="name = '%s' and mimeType = 'application/vnd.google-apps.folder'" % (folder),
+        pageSize=10,
+        fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    # Id of folder Photos_test
+    id_photos_test = items[0]['id']
+    results_photos = google_service.files().list(
+        q="'%s' in parents" % (id_photos_test),
+        pageSize=10,
+        fields="nextPageToken, files(id, name)").execute()
+    items_photos = results_photos.get('files', [])
+
+    # List folder Photos_test with name and id
+    if not items_photos:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items_photos:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+    return items_photos
