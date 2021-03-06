d903075b93dcb3f6f34a976f73dd2088f591a865
Patrick Hervieux <patrick.hervieux@museebolo.ch>
2021-03-06 18:40:09 +0100

    Rename a file

--- connect.py
+++ connect.py
@@ -36,7 +36,7 @@ def get_gdrive_service():
     return service
 
 
-def get_folder_id(google_service, folder):
+def get_photos_ids(google_service, folder):
     """Return list with a dict containing name and id of files from a given folder
     Parameter google_service is a googleapiclient.discovery.Resource class
     Parameter folder is a string containing folder name
@@ -66,3 +66,18 @@ def get_folder_id(google_service, folder):
             print(u'{0} ({1})'.format(item['name'], item['id']))
 
     return items_photos
+
+
+def rename_file(google_service, id_file, old_name, new_name):
+    """Rename files
+    Parameter google_service is a googleapiclient.discovery.Resource class
+    Parameter id_file is a string
+    Parameter old_name is a string
+    Parameter new_name is a string
+    """
+
+    copy_name = "{} | {}".format(old_name, new_name)
+    body = {"name": copy_name}
+    google_service.files().update(
+        fileId=id_file,
+        body=body).execute()
