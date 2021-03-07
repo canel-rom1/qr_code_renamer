9a6368d58493934aa095b2b9149da1836de25b56
Patrick Hervieux <patrick.hervieux@museebolo.ch>
2021-03-07 01:05:56 +0100

    Move permute functions qr_code_value and get_gdrive_service

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 
-import cv2
+from __future__ import print_function
+import connect
+import pickle
+import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
-def qr_code_value(img):
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+
+def get_gdrive_service():
+    """Return a googleapiclient.discovery.Resource class
+    Connect to my drive
     """
-    Return a string. Value is QR-code content
-    Parameter img : path to image
-    """
-    image = cv2.imread(str(img))
-    qr_code_detector = cv2.QRCodeDetector()
-    decoded_text, points, matrix = qr_code_detector.detectAndDecode(image)
-    return decoded_text
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens,
+    # and is created automatically when the authorization flow
+    # completes for the first time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save  the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('drive', 'v3', credentials=creds)
+    return service
