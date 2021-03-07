141dd1fc1c4ac5020bd878bce4d70c4174f037ce
Patrick Hervieux <patrick.hervieux@museebolo.ch>
2021-03-07 18:22:29 +0100

    Get line of a given inventory number in a spreadsheet

--- url_spreadsheets.py
+++ url_spreadsheets.py
@@ -1,3 +1,10 @@
+import sys
+
+
+SPREADSHEET_ID = '1IaCeVoiLyGOovfZAabt3h6zP2pEMef5jFWli55fGHPI'
+MATERIAL_ID = '496947566'
+
+
 def get_url(service, file_id):
     """
     Return URL of a file - string
@@ -9,3 +16,27 @@ def get_url(service, file_id):
         fileId=file_id,
         fields='webViewLink').execute()
     return url['webViewLink']
+
+
+# Je ne sais pas encore comment obtenir toutes les lignes d'une seule colonne
+def get_inventory_line(service, inventory_id):
+    """
+    Return a list with a single position
+    service : googleapiclient.discovery.Resource class
+    inventory_id : inventory id - str
+    """
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range='Matériel!B:C').execute()
+    position = list()
+    for index, value in enumerate(result['value']):
+        if value[0] == str(inventory_id):
+            # Don't count from 0
+            position.append(index + 1)
+
+    if len(position) > 2:
+        sys.exit("%s inventory with the same name. Exit" % (len(position)))
+
+    return position
